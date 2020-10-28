a8e72e7b3b5118cf6ae025c5b7d9a47f0e2f4769
Alexey Bilko <bilkoalexey67@gmail.com>
2020-10-28 11:09:16 +0200

    Добавьте файлы проекта.

---
+++ Laba2_C_Sharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, x;
+            Console.WriteLine("Enter value of parametr a: (it should be number value)");
+
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a > 0)
+                {
+                    if (a > 1)
+                    {
+                        x = (1 - a) / (2 * a);
+                    }
+                    else
+                    {
+                        x = (a - 1) / (2 * a);
+                    }
+                }
+                else
+                {
+                    x = Math.Log(Math.Sqrt(1 + Math.Pow(a, 2)), Math.E);
+                }
+                Console.WriteLine($"X --> {x}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
